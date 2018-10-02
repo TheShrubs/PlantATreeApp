afef0d4f70067408e6f1413872bc8175e298b5af
setavita <42175076+setavita@users.noreply.github.com>
2018-10-03 04:39:22 +1300

    altered files for draggable photos

--- plantATree/app/src/main/java/com/example/theshrubs/plantatree/activity/ViewingTreeActivity.java
+++ plantATree/app/src/main/java/com/example/theshrubs/plantatree/activity/ViewingTreeActivity.java
@@ -3,14 +3,18 @@ package com.example.theshrubs.plantatree.activity;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.theshrubs.plantatree.R;
 
-public class ViewingTreeActivity extends AppCompatActivity {
+public class ViewingTreeActivity extends AppCompatActivity implements View.OnDragListener{
 
     private ImageView takenPboto;
+    private ImageView dragPhoto;
     private Bitmap photo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,16 @@ public class ViewingTreeActivity extends AppCompatActivity {
 
         this.takenPboto = (ImageView) findViewById(R.id.housePhoto);
         this.photo = ViewItemActivity.getBitmap();
-
         this.takenPboto.setImageBitmap(photo);
 
+        this.dragPhoto = (ImageView) findViewById(R.id.itemDrop);
+        this.dragPhoto.setOnDragListener(this);
+
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        return false;
     }
 
 //    public void setTakenPboto(Bitmap photo){
