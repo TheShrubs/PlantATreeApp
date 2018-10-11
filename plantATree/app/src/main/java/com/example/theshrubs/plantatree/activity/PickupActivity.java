11e255d3f2cde75f7d832d75e7d5d7f0f0d6dfd2
DarthKidd <42362465+DarthKidd@users.noreply.github.com>
2018-10-12 02:17:21 +1300

    pickup complete

--- plantATree/app/src/main/java/com/example/theshrubs/plantatree/activity/PickupActivity.java
+++ plantATree/app/src/main/java/com/example/theshrubs/plantatree/activity/PickupActivity.java
@@ -1,6 +1,7 @@
 package com.example.theshrubs.plantatree.activity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +9,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.theshrubs.plantatree.R;
 
-public class PickupActivity extends AppCompatActivity implements View.OnClickListener{
+public class PickupActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private int USER_ID;
     private double TOTAL_COST;
@@ -26,23 +28,24 @@ public class PickupActivity extends AppCompatActivity implements View.OnClickLis
     private BottomNavigationMenu navigationControl;
     private BottomNavigationView navigationView;
     private Spinner spinner;
+    private ImageView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pickup);
 
-        //string array is located in values/strings called addresses - referenced from there
+//        string array is located in values/strings called addresses - referenced from there
 //        String[] address = {"22 Symonds St", "106 Great North Road", "43 Titirangi Road"};
-//        spinner = (Spinner) findViewById(R.id.spinner);
-//        //use an arrayAdapter to put string inside Spinner, pass in this context, the array from resources and a simple spinner layout
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.addresses, android.R.layout.simple_spinner_item);
-//        //have to pass the layout for the drop down - so pass in another simple layout
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        //pass the adapter
-//        spinner.setAdapter(adapter);
-//        //set On Item selected listener
-//        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        //use an arrayAdapter to put string inside Spinner, pass in this context, the array from resources and a simple spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.addresses, android.R.layout.simple_spinner_item);
+        //have to pass the layout for the drop down - so pass in another simple layout
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //pass the adapter
+        spinner.setAdapter(adapter);
+        //set On Item selected listener
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
         Bundle extras = getIntent().getExtras();
         USER_ID = extras.getInt("USER_ID");
@@ -84,24 +87,25 @@ public class PickupActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-//    @Override
-//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//        if(parent.getItemAtPosition(position).toString().equals("22 Symonds St")){
-//            //set image view
-//            Toast.makeText(this, "Symonds confirmed.", Toast.LENGTH_SHORT).show();
-//        }
-//        if(parent.getItemAtPosition(position).toString().equals("106 Great North Road")){
-//            //set image view
-//            Toast.makeText(this, "Great North Rd confirmed.", Toast.LENGTH_SHORT).show();
-//        }
-//        if(parent.getItemAtPosition(position).toString().equals("43 Titirangi Road")){
-//            //set image view
-//            Toast.makeText(this, "Titirangi confirmed.", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> parent) {
-//
-//    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        map = (ImageView) findViewById(R.id.map);
+        if(parent.getItemAtPosition(position).toString().equals("22 Symonds St")){
+            map.setImageResource(R.drawable.symondsst);
+            Toast.makeText(this, "Symonds confirmed.", Toast.LENGTH_SHORT).show();
+        }
+        if(parent.getItemAtPosition(position).toString().equals("106 Great North Road")){
+            map.setImageResource(R.drawable.greatnorthroad);
+            Toast.makeText(this, "Great North Rd confirmed.", Toast.LENGTH_SHORT).show();
+        }
+        if(parent.getItemAtPosition(position).toString().equals("43 Titirangi Road")){
+            map.setImageResource(R.drawable.titirangiroad);
+            Toast.makeText(this, "Titirangi confirmed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
